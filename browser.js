7f9942c5cd58017a80cbd39e47e803d264733986
Tom Shea <tom@shea.at>
2014-04-26 12:48:35 -0700

    Added a back button to the browser

--- browser.js
+++ browser.js
@@ -1,5 +1,6 @@
 function BrowserFakeWindow(menuname) {
 	this.url = "sneakyweb.com";
+	this.history = [];
 	FakeWindow.call(this, menuname);
 	register_ingame_links();
 }
@@ -22,12 +23,27 @@ BrowserFakeWindow.prototype.buildBody = function() {
 	
 	var container = document.createElement("div");
 	
+	this.backButton = document.createElement("a");
+	this.backButton.innerHTML = "&#9664;";
+	this.backButton.className = "backButton"
+	$(this.backButton).click(function() {
+		browser.back();
+	});
+	
+	
 	this.body = document.createElement("div");
 	this.body.innerHTML = this.getBody();
 	
 	container.appendChild(urlbar_form);
+	container.appendChild(this.backButton);
 	container.appendChild(this.body);
 	return container;
+}
+
+BrowserFakeWindow.prototype.back = function() {
+	if(this.history.length == 0)
+		return;
+	goToURL(this.history.pop(), true);
 }
 
 BrowserFakeWindow.prototype.getBody = function() {
@@ -41,8 +57,11 @@ BrowserFakeWindow.prototype.getBody = function() {
 	return bodies[this.url] || bodies["blocked"];
 }
 
-function goToURL(url) {
+function goToURL(url, ignoreHistory) {
 	var browser = openWindow("Browser");
+	if(!ignoreHistory && browser.url != url) {
+		browser.history.push(browser.url);
+	}
 	browser.url = url;
 	browser.body.innerHTML = browser.getBody();
 	register_ingame_links();
