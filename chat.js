cc450a71fc8628899cec5b4cdc39e111dbe88631
Tom Shea <tom@shea.at>
2014-04-26 00:08:48 -0700

    Initial friends system

---
+++ chat.js
@@ -0,0 +1,31 @@
+
+function ChatFakeWindow(menuname) {
+	FakeWindow.call(this, menuname);
+}
+$.extend(true, ChatFakeWindow.prototype, FakeWindow.prototype);
+
+ChatFakeWindow.prototype.buildBody = function() {
+	var container = document.createElement("div");
+	this.friendslist = document.createElement("ul");
+	this.friendslist.className = "friendslist";
+	
+	for(var name in friends) {
+		var item = friends[name].friendlist_elem = document.createElement("li");
+		item.className = "friend"
+		
+		var name_bar = document.createElement("span");
+		name_bar.className = "name";
+		name_bar.innerHTML = name;
+		
+		var profile_pic = document.createElement("img");
+		profile_pic.src = "images/" + friends[name].profile_picture;
+		profile_pic.className = "profile";
+		item.appendChild(profile_pic);
+		
+		item.appendChild(name_bar);
+		this.friendslist.appendChild(item);
+	}
+	
+	container.appendChild(this.friendslist);
+	return container;
+}
---
+++ friends.js
@@ -0,0 +1,13 @@
+var friends = {};
+
+friends["John"] = {
+	profile_picture: "john.png"
+};
+
+friends["Jimmy"] = {
+	profile_picture: "jimmy.png"
+};
+
+friends["xx420BigSmokerxx"] = {
+	profile_picture: "stoner.png"
+};
