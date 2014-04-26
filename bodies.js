4c419ddf6d04c7dead920e306ed67266f0bdfc64
Tom Shea <tom@shea.at>
2014-04-26 00:08:34 -0700

    Initial Browser bodies

---
+++ bodies.js
@@ -0,0 +1,5 @@
+bodies = {
+	"sneakyweb.com": "<h2>Welcome to SneakyWeb!</h2><p>The best in no-nonsense, minimal web browsing.</p><a href='http://testinglink.com' class='ingame_link'>TEST</a>",
+	"blocked": "<h3>Blocked by WorkFilter Plus</h3> Reason: 'Not relevant to current assignment'"
+
+}
